38bfa500c2fb7e6fc8cb7f85de3efa444c5db0f9
ashish sinha <ashishsinha14@gmail.com>
2016-12-28 20:55:46 -0500

    Updated Commit

--- CallApplyBindThis.js
+++ CallApplyBindThis.js
@@ -42,7 +42,7 @@ makeSound("meow meow");//O/P would be "meow meow" as call will execute produceSo
 var makeSound1 = Animal1.produceSound.bind(Cat);
 makeSound1("Hello");//O/P would be Meoooooooowwwwww as call will execute produceSound in "Cat" context where sound=Meoooooooowwwwww
 
-//*****************************************Another Example**********************************************************************
+//*****************************************Another Example To Understand Call,Apply and Bind**********************************************************************
 var person = {
     name: "James Smith",
     hello: function (thing) {
@@ -50,9 +50,9 @@ var person = {
     }
 }
 
-//call attaches this into function and executes the function immediately:
+//CALL: call attaches this into function and executes the function immediately:
 person.hello.call(person, "world"); // output: "James Smith says hello world"
 
-//bind attaches this into function and it needs to be invoked separately like this:
+//BIND: bind attaches this into function and it needs to be invoked separately like this:
 var helloFunc = person.hello.bind(person);
 helloFunc("world");  // output: "James Smith says hello world"
